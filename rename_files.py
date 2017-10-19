c10dec394f247cd41a02bfa63a9cf32cba440c7b
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-18 22:15:38 -0700

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -3,7 +3,15 @@ def rename_files():
     #(1) get file names from a folder
     file_list = os.listdir(r"C:\Users\ianle\Desktop\Volunteer - Professional - Adult\Professional Development\Udacity\Programming Foundations with Python\Secret Message Prank")
     print(file_list)
+    saved_path = os.getcwd()
+    print("Current Working Directory is "+saved_path)
+    os.chdir(r"C:\Users\ianle\Desktop\Volunteer - Professional - Adult\Professional Development\Udacity\Programming Foundations with Python\Secret Message Prank")
     
     #(2) for each file, rename filename
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
 
 rename_files ()
+
+    
