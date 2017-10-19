d13f89540eadbd55e627490034970398c567aaf1
Ian Lefcourte <ianlefcourte@gmail.com>
2017-10-18 21:42:28 -0700

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,5 @@
 import os
 def rename_files():
     #(1) get file names from a folder
-    os.listdir("C:\Users\ianle\Desktop\Volunteer - Professional - Adult\Professional Development\Udacity\Programming Foundations with Python\Lesson 2 of 3 - Use Functions\Secret Message Prank")
+    os.listdir(r"C:\Users\ianle\Desktop\Volunteer - Professional - Adult\Professional Development\Udacity\Programming Foundations with Python\Lesson 2 of 3 - Use Functions\Secret Message Prank")
     #(2) for each, rename file
